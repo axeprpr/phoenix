73a43c8e5d98307727feb85f6e92e46264b5e21a
Axe.Xu <axe3@qq.com>
2023-12-29 15:54:04 +0800

    stabilize installer script path handling

--- installation/setup.sh
+++ installation/setup.sh
@@ -1,29 +1,29 @@
-#!/bin/bash -x
+#!/bin/bash
 ###
  # @Description: 
  # @Date: 2023-03-31 17:35:54
  # @LastEditTime: 2023-12-23 01:24:58
  # @FilePath: \phoenix\installation\setup.sh
 ### 
-WORK_DIR=$(cd `dirname $0`; pwd)
-MODULE_DIR=/opt/astute_phoenix
-MODULE_COMMAND=/usr/bin/ph
+WORK_DIR=$(cd "$(dirname "$0")" && pwd)
+MODULE_DIR="/opt/astute_phoenix"
+MODULE_COMMAND="/usr/bin/ph"
 
-if [ `whoami` != "root" ];then  
+if [ "$(id -u)" -ne 0 ]; then
     echo "Need root."
-    exit -1  
+    exit 1
 fi
 
-if [ -d $MODULE_DIR ];then
-    rm -rf $MODULE_DIR
+if [ -d "${MODULE_DIR}" ]; then
+    rm -rf "${MODULE_DIR}"
 fi
 
-if [ -L $MODULE_COMMAND ];then
-    rm -f $MODULE_COMMAND
+if [ -L "${MODULE_COMMAND}" ] || [ -f "${MODULE_COMMAND}" ]; then
+    rm -f "${MODULE_COMMAND}"
 fi
 
-mkdir $MODULE_DIR
-cp -rf scripts utils ./phoenix.sh $MODULE_DIR
-chmod 755 $MODULE_DIR -R
+mkdir -p "${MODULE_DIR}"
+cp -rf scripts utils ./phoenix.sh "${MODULE_DIR}"
+chmod -R 755 "${MODULE_DIR}"
 
-ln -s ${MODULE_DIR}/phoenix.sh $MODULE_COMMAND
+ln -s "${MODULE_DIR}/phoenix.sh" "${MODULE_COMMAND}"
