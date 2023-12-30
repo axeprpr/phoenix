5ead175c083adb4ef62ba781340e3561c0aa9523
Axe.Xu <axe3@qq.com>
2023-12-30 15:31:09 +0800

    make build script fail fast on missing tools

--- build.sh
+++ build.sh
@@ -5,23 +5,30 @@
  # @LastEditTime: 2023-12-23 11:39:45
  # @FilePath: \phoenix\build.sh
 ### 
-WORK_DIR=$(cd `dirname $0`; pwd)
+set -euo pipefail
+
+WORK_DIR=$(cd "$(dirname "$0")" && pwd)
 
 VERSION_NO=1.0.0
 CODEDIR=phoenix
 
-[ -z $BUILD_NUMBER ] && BUILD_NUMBER=0
-[ -z $GIT_COMMIT ] && GIT_COMMIT="null"
+[ -z "${BUILD_NUMBER:-}" ] && BUILD_NUMBER=0
+[ -z "${GIT_COMMIT:-}" ] && GIT_COMMIT="null"
 
-cd $WORK_DIR
+cd "${WORK_DIR}"
 # clean bins
-rm -f $WORK_DIR/*.bin
-rm -f $WORK_DIR/*.md5
-makeself=$WORK_DIR/makeself/makeself.sh
+rm -f "${WORK_DIR}"/*.bin
+rm -f "${WORK_DIR}"/*.md5
+makeself="${WORK_DIR}/makeself/makeself.sh"
+
+if [ ! -x "${makeself}" ]; then
+    echo "makeself not found: ${makeself}"
+    exit 1
+fi
 
 # make shadowman.bin
-chmod +x $makeself
-chmod +x $WORK_DIR/installation/setup.sh
-$makeself --gzip $WORK_DIR/installation phoenix.$BUILD_NUMBER.bin phoenix  ./setup.sh
-chmod +x phoenix.$BUILD_NUMBER.bin
-md5sum phoenix.$BUILD_NUMBER.bin > phoenix.$BUILD_NUMBER.bin.md5
+chmod +x "${makeself}"
+chmod +x "${WORK_DIR}/installation/setup.sh"
+"${makeself}" --gzip "${WORK_DIR}/installation" "phoenix.${BUILD_NUMBER}.bin" phoenix ./setup.sh
+chmod +x "phoenix.${BUILD_NUMBER}.bin"
+md5sum "phoenix.${BUILD_NUMBER}.bin" > "phoenix.${BUILD_NUMBER}.bin.md5"
